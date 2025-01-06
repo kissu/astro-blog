a8f56ffc3d5d4032b768e707dcebd709c4532faa
Konstantin BIFERT <hello@kissu.io>
2025-01-06 22:42:38 +0100

    chore(init): bump deps + format + setup basic temporary consts + remove non-english blog posts

--- src/consts.ts
+++ src/consts.ts
@@ -1,17 +1,16 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Koibumi Blog';
+export const SITE_TITLE = "Neevaaa's Blog";
 
-export const SITE_DESCRIPTION = 'A blog about SolidJS, Astro, and koibumi design.';
+export const SITE_DESCRIPTION = 'My journey as a web developer starts here! 💖';
 
 export const FAVICON_SRC = '/favicon.svg';
 
 export const AVATAR_SRC = '/avatar.avif';
 
-export const BIO = "This is a demo site for Koibumi Astro Theme. Fork this from github and start your own blog!";
+export const BIO = "Hey, I'm neevaaa! :3";
 
-export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'glass'
+export const GLOBAL_STYLE: 'quartz' | 'glass' | 'lightGlass' = 'lightGlass'
 
 export const COPYRIGHT_NAME = 'Koibumi Design';
-
